99e36e372e276854086eb85db648571467f732a3
EdwardBoy24 <lalogamer108@gmail.com>
2026-05-31 13:40:01 -0400

    Se modifico la clase main, para que ahora el programa funcione correctame con el patron ADAPTER, finalizando esta primera parte de la evaluacion

--- actividad_robott/src/game/Main.java
+++ actividad_robott/src/game/Main.java
@@ -1,16 +1,45 @@
 package game;
-//import external.robo.EnemyRobot;
+
+// Ya no necesitamo importar external.robo.EnemyRobot aquí, lo hicimos de manera interna en su clase
+
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         Enemy enemy1 = new Player();
         Enemy enemy2 = new Player();
         
-
-        enemy2.takeDamage(enemy1.attack(enemy2));
-        System.out.println(enemy2.getHealth());
+        // asi deberia verse visualmente creemos
         
-        //robotEnemy.attack(enemy2); --> Objetivo
+        
+        System.out.println("- El primer enemigo ataca -");
+        Thread.sleep(2000); 
+        enemy2.takeDamage(enemy1.attack(enemy2));
+        Thread.sleep(2000); 
+        System.out.println("Salud de enemigo 2: " + enemy2.getHealth());
+        Thread.sleep(2000); 
+  
+        //para que se entienda que es un juego
+        System.out.println("- El segundo enemigo ataca -");
+        Thread.sleep(2000); 
+        enemy1.takeDamage(enemy2.attack(enemy1));
+        Thread.sleep(2000); 
+        System.out.println("Salud de enemigo 1: " + enemy1.getHealth());
+        Thread.sleep(2000); 
 
-        }
+        System.out.println("\n- Liberando al robot adaptado -");
+        Thread.sleep(2000); 
+        
+        // Instanciamos nuestro adaptador
+        // internamente creara al robot y ejecutara su bootSequence
+        Enemy  robotEnemy= new RobotAdapter(); 
+
+        // Le ordenamos interactuar con los demas objetos, haber si funciona
+        System.out.println("- El robot ataca -"); 
+        robotEnemy.attack(enemy2);
+        Thread.sleep(2000);
+        
+        
+        System.out.println("\nSalud de enemigo 2 tras ser aplastado: " + enemy2.getHealth());
+    }
 }
